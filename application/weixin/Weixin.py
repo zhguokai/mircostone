262b6a6ab8ee6534a2a3ae4fc93eb49e28a929a1
zhgk <zhguokai@163.com>
2014-11-27 20:55:32 +0800

    增加微信接口

---
+++ application/weixin/Weixin.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import hashlib
+
+from tornado.web import RequestHandler
+
+
+class AccessWeixinHandler(RequestHandler):
+    def get(self):
+        signature = self.get_argument("signature")
+        timestamp = self.get_argument("timestamp")
+        nonce = self.get_argument("nonce")
+        echostr = self.get_argument("echostr")
+
+        print("weixin: %s : %s : %s" % signature % timestamp % nonce)
+
+        token = "zgkAndHxh"
+
+        listWeixin = list[token, timestamp, nonce]
+        listWeixin.sort()
+        sha1 = hashlib.sha1()
+        map(sha1.update, listWeixin)
+        hashcode = sha1.hexdigest()
+
+        if hashcode == signature:
+            return echostr
+        else:
+            return False
+
+
+__author__ = 'zhgk'
