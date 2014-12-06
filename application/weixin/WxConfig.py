a4a94109a7cb4c7f27e1e0450a0f27b5069405fb
zhgk <zhguokai@163.com>
2014-12-06 18:14:32 +0800

    修改为测试号

--- application/weixin/WxConfig.py
+++ application/weixin/WxConfig.py
@@ -7,9 +7,9 @@
 
 class WeiXinConfig:
     # 应用ID
-    APP_ID = 'wx36d0f120ffba09b1'
+    APP_ID = 'wx497d167c202042f1'
     # 应用密钥匙
-    APP_SEC = "c19f328910d187fbf671d72afc25e418"
+    APP_SEC = "971b1d507404e04fb44f4da06744eb43"
     # 访问凭证获取地址
     APP_ACCESS_TOKEN_URL = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' \
                            + APP_ID + '&secret=' + APP_SEC
