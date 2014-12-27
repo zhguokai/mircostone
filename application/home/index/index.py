ac8edf18df9068a1bfb58473d3385c34e869dce4
相濡HH <zhguokai@163.com>
2014-12-28 00:46:31 +0800

    测试应用中图片生成文本

--- application/home/index/index.py
+++ application/home/index/index.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
+import os
 from tornado.web import RequestHandler
+from tornado.web import StaticFileHandler
 
 from PIL import Image,ImageDraw,ImageFont
-
 from application.db.connect.postgresql_conn import PostDBConn
 from application.Logger import weixinLogger
 
@@ -16,19 +17,19 @@ class IndexHandler(RequestHandler):
         #url = "http://"+self.request.argrument["url"]
         #self.redirect(url.decode(encoding='utf8'))
         # templeate = Template("home/index.html")
-        pcon = PostDBConn()
-        sqlstr = "CREATE TABLE public.Q2test (id serial PRIMARY KEY, num integer, data varchar);"
+        #pcon = PostDBConn()
+        #sqlstr = "CREATE TABLE public.Q2test (id serial PRIMARY KEY, num integer, data varchar);"
         #pcon.exec_sql(sqlstr)
         try:
-            font = ImageFont.truetype('simsun.ttc',88)
-            img = Image.open("qt2.png")
+            font = ImageFont.truetype("application/static/fonts/simsun.ttc",88)
+            img = Image.open("application/static/img/qt2.png")
             draw = ImageDraw.Draw(img)
             draw.text((200,100),u'你好,世界!',(23,55,99),font = font)
             draw.text((260,140),unicode('你好','utf-8'),(678,123,234),font = font)
-            img.save("jpeg.png",'PNG')
+            img.save("application/static/img/jpeg.png",'PNG')
             indexLog.info("成功了")
         except Exception as e:
-            indexLog.info(e.message)
+            indexLog.info('失败了'+e.message)
 
         self.render('home/yyy.html')
 
--- application/test/tecdemo/imgWrit.py
+++ application/test/tecdemo/imgWrit.py
@@ -2,8 +2,8 @@
 from PIL import Image,ImageDraw,ImageFont
 
 font = ImageFont.truetype('simsun.ttc',88)
-img = Image.open("tt.jpg")
+img = Image.open("qt2.png")
 draw = ImageDraw.Draw(img)
 draw.text((200,100),u'你好,世界!',(23,55,99),font = font)
 draw.text((260,140),unicode('你好','utf-8'),(678,123,234),font = font)
-img.save("jpeg.jpg",'JPEG')
+img.save("jpeg.png",'PNG')
