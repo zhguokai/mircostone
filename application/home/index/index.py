b5fb8b4e4e626e8307c8fcba770f663d092f09cc
相濡HH <zhguokai@163.com>
2014-12-28 13:22:04 +0800

    动态生成图片

--- application/home/index/index.py
+++ application/home/index/index.py
@@ -14,16 +14,7 @@ class IndexHandler(RequestHandler):
         # pcon = PostDBConn()
         #sqlstr = "CREATE TABLE public.Q2test (id serial PRIMARY KEY, num integer, data varchar);"
         #pcon.exec_sql(sqlstr)
-        try:
-            font = ImageFont.truetype("application/static/fonts/simsun.ttc",88)
-            img = Image.open("application/static/img/qt2.png")
-            draw = ImageDraw.Draw(img)
-            draw.text((200,100),u'你好,世界!',(23,55,99),font = font)
-            draw.text((260,140),unicode('你好','utf-8'),(678,123,234),font = font)
-            img.save("application/static/img/jpeg.png",'PNG')
-            indexLog.info("成功了")
-        except Exception as e:
-            indexLog.info('失败了' + e.message)
+
 
         self.render('home/yyy.html')
 
