8f24a3c1b632c55f7950a7ef27a1baae5eeaf875
zhgk <zhguokai@163.com>
2015-01-08 23:56:53 +0800

    修正日志格式

--- application/Logger.py
+++ application/Logger.py
@@ -12,7 +12,6 @@ class AppLogger(object):
     # 定义SysLogger实例
     __instance = None
 
-
     def __init__(self):
         """
             初始化日志工具
@@ -24,7 +23,7 @@ class AppLogger(object):
         # 定义日志显示格式
         formatter = logging.Formatter("%(asctime)s  - %(levelname)s - %(message)s")
         # 定义显示类型
-        filehandler = logging.FileHandler(filename,mode = 'a',encoding = 'utf-8',delay = True)
+        filehandler = logging.FileHandler(filename, mode='a', encoding='utf-8', delay=True)
         # 定义日志显示格式
         filehandler.setFormatter(formatter)
         # 创建日志类
@@ -35,7 +34,6 @@ class AppLogger(object):
         logg.addHandler(filehandler)
         # 定义日志句柄
         self.__loghandle = logg
-
 
     @staticmethod
     def get_loghandle(classname):
@@ -55,9 +53,4 @@ class AppLogger(object):
         return AppLogger.__instance.__loghandle
 
 
-if __name__ == "__main__":
-    wlog = AppLogger.get_loghandle("AppLogger")
-    wlog.info("hello %s" % wlog)
-    elog = AppLogger.get_loghandle("AppLogger")
-    elog.info("hello %s" % elog)
 __author__ = 'zhgk'
