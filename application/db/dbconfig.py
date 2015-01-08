66c5fc0aaaf5e488a8d417e9c749f3d47d2d6cf8
zhgk <zhguokai@163.com>
2015-01-08 23:55:25 +0800

    增加mysql数据库连接方法

--- application/db/dbconfig.py
+++ application/db/dbconfig.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-数据库配置文件
-
+数据库连接配置类
 """
-#POSTGRESQL数据库配置信息
-POSTGRESQL_DB_HOST="127.0.0.1"
-POSTGRESQL_DB_PORT="5432"
-POSTGRESQL_DB_NAME="weixin"
-POSTGRESQL_DB_USER="zhgk"
-POSTGRESQL_DB_PASS="bugaosuni"
-#MONGODB
-#MYSQL
+
+
+class MysqlConfig(object):
+    """
+        数据库配置信息
+    """
+    # IP等地址未替换
+    MYSQL_HOST = "127.0.0.1"
+    MYSQL_PORT = 3306
+    MYSQL_DBNAME = "mircostone"
+    MYSQL_USER = "root"
+    MYSQL_PASS = "123456"
+    MYSQL_MIN_CON = 10
+    MYSQL_MAX_CON = 50
+    MYSQL_CHAR_SET = "utf8"
+    MYSQL_COLLECTION = "utf8_general_ci"
 
 
 __author__ = 'zhgk'
