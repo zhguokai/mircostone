a4678b7f95453bb85b019d9ebab09e96c35a1bea
zhgk <zhguokai@163.com>
2015-01-10 01:32:19 +0800

    增加登录页面，

---
+++ app/bizc/login/login_bzc.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+登录业务处理类，用于验证用户密码，存储Session，返回菜单等
+"""
+
+
+class LoginBiz(object):
+    """
+        登录业务处理
+    """
+
+    def __init__(self):
+        """
+        初始化
+        :return:
+        """
+
+    def validate_user(self, username, userpass):
+        """
+        验证有户信息
+        :param username:
+        :param userpass:
+        :return True Or False:
+        """
+
+        query_user_str = """
+            select user_id from b_wx_users WHERE user_code=%s AND user_pass =%s
+        """
+
+
+__author__ = 'zhgk'
