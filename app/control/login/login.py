3903f230f58970b14e0cf62e9f887174b095eefb
zhgk <zhguokai@163.com>
2015-01-12 23:47:49 +0800

    修正用户密码变量

--- app/control/login/login.py
+++ app/control/login/login.py
@@ -2,10 +2,10 @@
 
 from tornado.web import RequestHandler
 
-from app.view.view_path import HTML_PATH
-
 from common.log.Logger import AppLogger
 from common.util.encode import EncodePass
+from app.view.view_path import HTML_PATH
+from app.bizc.login.login_bzc import LoginBiz
 
 
 loginLog = AppLogger.get_loghandle(__name__)
@@ -25,21 +25,31 @@ class LoginHandler(RequestHandler):
         :return:
         """
 
-        self.render(HTML_PATH.get("Login.login"))
+        self.render(HTML_PATH.get("Login.login"), errmsg=None)
 
     def post(self):
         """
         post方法，接收用户名，密码
         :return:
         """
+        loging_biz = LoginBiz()
         username = self.get_body_argument("username")
-        userpass = self.get_body_argument("userpass")
+        userpass = self.get_body_argument("password")
 
         if username is not None and userpass is not None:
             userpass = EncodePass.encode_sha512(userpass)
+
+            query_params = (username, userpass)
+            result = loging_biz.validate_user(query_params)
+            if result is not None and len(result.rows) == 1:
+                self.render(HTML_PATH.get("Main.main"), userinfo=result.rows[0])
+            else:
+                errmsg = u"您输入的用户名或密码不正确"
+                self.render(HTML_PATH.get("Login.login"), errmsg=errmsg)
         else:
             #
-            self.render(HTML_PATH.get("Login.login"))
+            errmsg = u"您输入的用户名密码不正确"
+            self.render(HTML_PATH.get("Login.login"), errmsg=errmsg)
 
 
 __author__ = 'zhgk'
